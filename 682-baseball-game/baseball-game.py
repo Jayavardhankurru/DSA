337fb2e898ef367c953270b68b6cb4e806ae877d
Jayavardhankurru <127181918+Jayavardhankurru@users.noreply.github.com>
2026-01-16 11:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (7.18%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        score = []
+        for operation in operations:
+            if operation == '+':
+                score.append(score[-1] + score[-2])
+            elif operation == 'D':
+                score.append(score[-1] * 2)
+            elif operation == 'C':
+                score.pop()
+            else:
+                score.append(int(operation))
+        return sum(score)

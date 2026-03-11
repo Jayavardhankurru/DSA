2cc313be1947d032bda55eb22dca639fe9d77470
Jayavardhankurru <127181918+Jayavardhankurru@users.noreply.github.com>
2026-03-11 14:55:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (90.95%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return self.dfsHeight(root) != -1
+    def dfsHeight(self, root):
+        if not root:
+            return 0
+        lh = self.dfsHeight(root.left)
+        if lh == -1:
+            return -1
+        rh = self.dfsHeight(root.right)
+        if rh == -1:
+            return -1
+        if abs(rh - lh) > 1:
+            return -1
+        return 1 + max(lh, rh)
